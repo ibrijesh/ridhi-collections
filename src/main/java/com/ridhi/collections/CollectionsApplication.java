b5eac18ee0da19798dfa057d1f98a136b030ef5a
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-29 17:35:36 +0530

    Queue - using ArrayDeque class

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -7,15 +7,16 @@ public class CollectionsApplication {
     public static void main(String[] args) {
 
         // FIFO queue
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> queue1 = new ArrayDeque<>();
+        queue1.offer(1);
+        queue1.offer(2);
+        queue1.offer(3);
 
-        queue.offer(1);
-        queue.offer(2);
-        queue.offer(3);
 
-        System.out.println(queue.peek());
-        System.out.println(queue.poll());
-        System.out.println(queue.peek());
+        System.out.println(queue1.peek());
+        System.out.println(queue1.poll());
+        System.out.println(queue1.peek());
+
 
     }
 
