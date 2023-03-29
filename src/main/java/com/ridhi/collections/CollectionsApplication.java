b57ac8552992ac56c6221059f8215e0d706892b7
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-29 16:27:24 +0530

    *List- ListIterator()
    next() - first print next move to next element.
    previous() - first move to prev element then print.

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -1,35 +1,23 @@
 package com.ridhi.collections;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class CollectionsApplication {
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(4);
-        list2.add(5);
-        list2.add(6);
-
-        list2.addAll(list);
-
-        // <4,5,6,1,2,3>
-        List<Integer> list3 = list2.subList(1, 4);
-        list3.set(0, 100);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
 
 
-        System.out.println(list);
-        System.out.println(list2);
-        System.out.println(list3);
+        // [1,2,3]
+        ListIterator<Integer> iterator = linkedList.listIterator();
+
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.previous());
 
     }
 
