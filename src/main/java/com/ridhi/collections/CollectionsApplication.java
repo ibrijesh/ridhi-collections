3240c488074939df045986f0bc2258979a7d58f7
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-29 16:16:32 +0530

    *List - subList()
    subList() creates sallow copy , changes will reflect in reference class also.

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -18,16 +18,18 @@ public class CollectionsApplication {
         List<Integer> list2 = new ArrayList<>();
         list2.add(4);
         list2.add(5);
-        list2.add(7);
+        list2.add(6);
 
         list2.addAll(list);
+
+        // <4,5,6,1,2,3>
+        List<Integer> list3 = list2.subList(1, 4);
+        list3.set(0, 100);
 
 
         System.out.println(list);
         System.out.println(list2);
-
-        for (var x : list2)
-            System.out.println(x);
+        System.out.println(list3);
 
     }
 
