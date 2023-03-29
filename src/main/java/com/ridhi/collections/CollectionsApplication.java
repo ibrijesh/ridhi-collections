1c431554c1251ec224b5f546eb7e22ef8bc072c0
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-29 15:54:55 +0530

    List- ArrayList()
      add() and iterate

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -1,26 +1,26 @@
 package com.ridhi.collections;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CollectionsApplication {
 
     public static void main(String[] args) {
 
-        OurGenericList<Integer> list = new OurGenericList<>();
+        List<Integer> list = new ArrayList<>();
+
         list.add(1);
         list.add(2);
         list.add(3);
-
 
         Iterator<Integer> iterator = list.iterator();
 
         while (iterator.hasNext())
             System.out.println(iterator.next());
 
-        System.out.println();
-
-
-        // using one line code //
+        /*   OR      */
 
         for (var x : list)
             System.out.println(x);
