2df9b25ab2f27e30b3f39a2676b2bc6f0c2cdca2
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-29 14:31:56 +0530

    Why Cannot iterate over list?   bcz data members are private.
    Should we make it public ?
      No it's bad idea, it will invalid OOP's encapsulation.

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -4,7 +4,19 @@ public class CollectionsApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        OurGenericList<Integer> list = new OurGenericList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        // it's not good way to use .get method , because maybe in future instead of list some other data structure is used and .get method is not applicable, OOP's polymorphism will not be achieved.
+
+        System.out.println(list.getItemAtIndex(3));
+
+
+        // We have not implemented iterable //
+        for (int x : list)
+            System.out.println(x);
 
     }
 
---
+++ src/main/java/com/ridhi/collections/OurGenericList.java
@@ -0,0 +1,20 @@
+package com.ridhi.collections;
+
+public class OurGenericList<T> {
+    private T[] items;
+    private int size;
+
+    public OurGenericList() {
+        size = 0;
+        items = (T[]) new Object[100];
+    }
+
+    public void add(T item) {
+        items[size++] = item;
+    }
+
+    public T getItemAtIndex(int index) {
+        return items[index];
+    }
+
+}
