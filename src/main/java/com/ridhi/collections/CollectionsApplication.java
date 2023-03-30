35f1c88973df381ef5f0cfaea541193dfa972346
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-30 14:11:51 +0530

    Set - using LinkedHashSet class
      - insertion order is maintained

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -7,25 +7,23 @@ public class CollectionsApplication {
     public static void main(String[] args) {
 
         // Set
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(2);
+        Set<Integer> set = new LinkedHashSet<>();
         set.add(3);
         set.add(1);
+        set.add(2);
+        set.add(1);
+        set.add(7);
 
 
         Set<Integer> set2 = new HashSet<>();
         set2.add(4);
         set2.add(3);
         set2.add(5);
+        set2.add(2);
 
         System.out.println(set);
         System.out.println(set2);
 
-        set.addAll(set2);
-        System.out.println("After addAll()");
-        System.out.println(set);
-        System.out.println(set2);
 
     }
 
