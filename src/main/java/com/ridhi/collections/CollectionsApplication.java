74cf7d622c6fe64535226c57aba6d74a75bcdbee
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-30 13:41:33 +0530

    Priority Queue -  Custom Comparator

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -6,16 +6,12 @@ public class CollectionsApplication {
 
     public static void main(String[] args) {
 
-        List<StudentMarks> stMArks = new ArrayList<>();
-        stMArks.add(new StudentMarks(100, 50));
-        stMArks.add(new StudentMarks(40, 70));
-        stMArks.add(new StudentMarks(66, 88));
-        stMArks.add(new StudentMarks(160, 99));
-        stMArks.add(new StudentMarks(23, 34));
-
-
         // Priority Queue   - sort according to min math marks
-        PriorityQueue<StudentMarks> pq = new PriorityQueue<>(stMArks);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new MyCustomComparator());
+
+        pq.offer(1);
+        pq.offer(2);
+        pq.offer(3);
 
 
         while (!pq.isEmpty())
