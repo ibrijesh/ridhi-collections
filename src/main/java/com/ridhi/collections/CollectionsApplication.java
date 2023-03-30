229974d6aa8ebf98021bc1cdb8fe422c8c044459
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-30 19:00:15 +0530

    Map - adjacency list for graph questions

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -6,18 +6,37 @@ public class CollectionsApplication {
 
     public static void main(String[] args) {
 
-        // Map
-        Map<String, Integer> map = new HashMap<>();
-        map.put("shivam", 1);
-        map.put("brijesh", 2);
-        map.put("rahul", 3);
+        // Map as graph
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        /*
+            1->2
+            3->5
+            1-3
+        */
+
+//        if (graph.get(1) == null)
+//            graph.put(1, new ArrayList<>());
+
+        // 1 line code
+        graph.computeIfAbsent(1, f -> new ArrayList<>()).add(2);
+        graph.computeIfAbsent(3, f -> new ArrayList<>()).add(5);
+        graph.computeIfAbsent(1, f -> new ArrayList<>()).add(3);
 
 
-        System.out.println(map);
-        System.out.println(map.getOrDefault("rahul", -1));
+        // For Traversing graph, we need to convert Map to Set as Map cannot be traversed//
 
-        // no null pointer issue
-        System.out.println(map.getOrDefault("ashish", -1));
+        Set<Map.Entry<Integer, List<Integer>>> entrySet = graph.entrySet();
+
+        for (Map.Entry<Integer, List<Integer>> entry : entrySet) {
+
+            System.out.print(entry.getKey() + "-->");
+
+            for (Integer edge : entry.getValue())
+                System.out.print(edge + " ");
+            System.out.println();
+        }
+
 
     }
 
