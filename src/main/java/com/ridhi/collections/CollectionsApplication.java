8f990e667545e9911863eeee597164e3d68614aa
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-30 18:39:44 +0530

    Map - get() but can cause null pointer issue
      - getOrDefault recommended

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -7,15 +7,26 @@ public class CollectionsApplication {
     public static void main(String[] args) {
 
         // Map
-        Map<String, Integer> map = new HashMap<>();
-        map.put("shivam", 1);
-        map.put("brijesh", 2);
-        map.put("rahul", 3);
+        Map<String, StudentMarks> map = new HashMap<>();
+        map.put(
+                "rahul",
+                new StudentMarks(44, 67)
+        );
+        map.put(
+                "brijesh",
+                new StudentMarks(64, 87)
+        );
+        map.put(
+                "shivam",
+                new StudentMarks(74, 64)
+        );
 
 
         System.out.println(map);
-        System.out.println(map.containsKey("rahul"));
-        System.out.println(map.containsKey("ashish"));
+        System.out.println(map.get("rahul").getMaths());
+
+        // null pointer issue
+        System.out.println(map.get("ashish").getMaths());
 
     }
 
