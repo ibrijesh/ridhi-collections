ee17e13572a9f0ddf187823e996c71ec2a03be78
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-31 13:00:25 +0530

    TreeMap

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -6,36 +6,13 @@ public class CollectionsApplication {
 
     public static void main(String[] args) {
 
-        // Map as graph
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        // TreeMap
+        NavigableMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(1, "brijesh");
+        treeMap.put(3, "shivam");
+        treeMap.put(2, "rahul");
 
-        /*
-            1->2
-            3->5
-            1-3
-        */
-
-//        if (graph.get(1) == null)
-//            graph.put(1, new ArrayList<>());
-
-        // 1 line code
-        graph.computeIfAbsent(1, f -> new ArrayList<>()).add(2);
-        graph.computeIfAbsent(3, f -> new ArrayList<>()).add(5);
-        graph.computeIfAbsent(1, f -> new ArrayList<>()).add(3);
-
-
-        /* second way to traverse */
-        Set<Integer> keySets = graph.keySet();
-
-        for (Integer key : keySets) {
-            System.out.print(key + "-->");
-
-            for (Integer edge : graph.get(key))
-                System.out.print(edge + " ");
-            System.out.println();
-        }
-
-
+        System.out.println(treeMap);
 
     }
 
