9ba0c237c8f8b0bdb8ddfaa6ff6496669d1efd2c
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-31 13:14:12 +0530

    Sorting - convert Array to List

--- src/main/java/com/ridhi/collections/CollectionsApplication.java
+++ src/main/java/com/ridhi/collections/CollectionsApplication.java
@@ -7,10 +7,12 @@ public class CollectionsApplication {
     public static void main(String[] args) {
 
         Integer arr[] = new Integer[]{1, 3, 4, 2, 6, 8, 7};
-        Arrays.sort(arr);
 
-        for(Integer x:arr)
-            System.out.println(x);
+        // convert Array to List //
+        List<Integer> list = Arrays.asList(arr);
+
+        System.out.println(list);
+
     }
 
 }
